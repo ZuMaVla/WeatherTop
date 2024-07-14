e92ddc29660cd86853e4a16f58aed92bd3bfede0
ZuMaVla <marinazubialevich@gmail.com>
2024-07-14 16:52:29 +0100

    Evolved to <Baseline>-version

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ import express from "express";
 import { dashboardController } from "./controllers/dashboard-controller.js";
 import { aboutController } from "./controllers/about-controller.js";
 import { mainController } from "./controllers/main-controller.js";
+import { stationController } from "./controllers/station-controller.js";
 
 export const router = express.Router();
 
@@ -9,3 +10,4 @@ router.get("/", mainController.index);
 router.get("/dashboard", dashboardController.index);
 router.get("/about", aboutController.index);
 router.post("/dashboard/addstation", dashboardController.addStation);
+router.get("/station/:stationId", stationController.index)
