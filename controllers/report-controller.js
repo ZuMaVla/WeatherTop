b53cac1c8b730af481c38961a0a0f61a5a0de7e3
ZuMaVla <marinazubialevich@gmail.com>
2024-07-15 19:34:53 +0000

    🐵🛣 Checkpoint ./controllers/report-controller.js:68521061/1223

--- controllers/report-controller.js
+++ controllers/report-controller.js
@@ -1,1 +1,34 @@
-import
+import { stationStore } from "../models/station-store.js";
+import { reportStore } from "../models/report-store.js";
+
+export const reportController = {
+  async index(request, response) {
+    const playlist = await playlistStore.getPlaylistById(request.params.id);
+    const viewData = {
+      title: "Playlist",
+      playlist: playlist,
+    };
+    response.render("playlist-view", viewData);
+  },
+
+  async addTrack(request, response) {
+    const playlist = await playlistStore.getPlaylistById(request.params.id);
+    const newTrack = {
+      title: request.body.title,
+      artist: request.body.artist,
+      duration: Number(request.body.duration),
+    };
+    console.log(`adding track ${newTrack.title}`);
+    await trackStore.addTrack(playlist._id, newTrack);
+    response.redirect("/playlist/" + playlist._id);
+  },
+  
+  async deleteTrack(request, response) {
+    const playlistId = request.params.playlistid;
+    const trackId = request.params.trackid;
+    console.log(`Deleting Track ${trackId} from Playlist ${playlistId}`);
+    await trackStore.deleteTrack(request.params.trackid);
+    response.redirect("/playlist/" + playlistId);
+  },
+
+};
