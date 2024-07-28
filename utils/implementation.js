0b09787b2a82e19cb37c1e94744ef05680ef31ac
ZuMaVla <marinazubialevich@gmail.com>
2024-07-28 12:56:41 +0000

    🌚💏 Checkpoint ./utils/implementation.js:68521061/258 ./models/reports.json:68521061/4

--- utils/implementation.js
+++ utils/implementation.js
@@ -7,12 +7,12 @@ import dayjs from 'dayjs';
 export async function prepareSummary(stationId) {
   const stationToView = await stationStore.getStationById(stationId);
   const stationReports = await reportStore.getReportsByStationId(stationId);
-  let currentWeatherCode = await WCCs.getWeatherByCode(100);
+  let currentWeatherCode = 100;
   if (stationReports.length > 0) {
     let temp = stationReports[stationReports.length - 1];
     currentWeatherCode = temp.code;
     if (stationReports.length > 1) {
-      for (let i = stationReports.length - 2; i < 0; i--) {
+      for (let i = stationReports.length - 2; i >= 0; i--) {
         if (dayjs(stationReports[i].reportDate).isAfter(dayjs(temp.reportDate))) {
           temp = stationReports[i];
           currentWeatherCode = temp.code;
@@ -29,6 +29,7 @@ export async function prepareSummary(stationId) {
   const maxW = stationStore.getParam(stationToView, "windSpeed", "max");
   const minP = stationStore.getParam(stationToView, "pressure", "min");
   const maxP = stationStore.getParam(stationToView, "pressure", "max");
+  currentWeatherCode = await WCCs.getWeatherByCode(currentWeatherCode);
   stationToView.attributes = {
     minTemp: minT,
     maxTemp: maxT,
