ea9f23af82e7687d50d5aa3d2d9334de177e9aa5
ZuMaVla <marinazubialevich@gmail.com>
2024-08-25 13:46:29 +0000

    🏙🛳 Checkpoint ./controllers/dashboard-controller.js:68521061/2128

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -8,27 +8,32 @@ import { prepareSummary } from "../utils/implementation.js";
 
 export const dashboardController = {
   async index(request, response) {
-    if (cookies)
-    const loggedInUser = await accountsController.getLoggedInUser(request);
-    const userStations = await stationStore.getStationByUserId(loggedInUser._id);
-    //const stationsAlphabeticallySorted = userStations.sort("name");
-    
-    let temp;
-    for (let i = 0; i < userStations.length; i++) {
-      temp = await prepareSummary(userStations[i]._id);
-      console.log('Current Station:', JSON.stringify(temp, null, 2));
+if (cookies && cookies.weathertop_user_token && cookies.weathertop_user_token.trim() !== "") {
+      const loggedInUser = await accountsController.getLoggedInUser(request);
+      const userStations = await stationStore.getStationByUserId(loggedInUser._id);
+      //const stationsAlphabeticallySorted = userStations.sort("name");
 
-      userStations[i].attributes = temp.attributes;
-    };
-    
-    const viewData = {
-      title: "Station Dashboard",
-      stations: userStations,
-      //userName: loggedInUser.firstName,
-      user: loggedInUser,
-    };
-    console.log("dashboard rendering");
-    response.render("dashboard-view", viewData);
+      let temp;
+      for (let i = 0; i < userStations.length; i++) {
+        temp = await prepareSummary(userStations[i]._id);
+        console.log('Current Station:', JSON.stringify(temp, null, 2));
+
+        userStations[i].attributes = temp.attributes;
+      };
+
+      const viewData = {
+        title: "Station Dashboard",
+        stations: userStations,
+        //userName: loggedInUser.firstName,
+        user: loggedInUser,
+      };
+      console.log("dashboard rendering");
+      response.render("dashboard-view", viewData);  
+    }
+    else {
+      console.log("No user is logged in");
+      response.redirect("/");  
+    }
   },
   
   async addStation(request, response) {
