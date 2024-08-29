65e941cdc16c5dd2d636402d03c2ef277c8a91e7
ZuMaVla <marinazubialevich@gmail.com>
2024-08-29 21:39:01 +0000

    ⛴🏁 Checkpoint ./utils/implementation.js:68521061/29

--- utils/implementation.js
+++ utils/implementation.js
@@ -65,7 +65,7 @@ export async function prepareChartData(stationId) {
   let tmp;
   
   for (let i = 0; i < stationReports.length; i++) {
-    tmp = dayjs(stationReports[i].reportDate);
+    tmp = dayjs(stationReports[i].reportDate, "YYYY/MM/DD HH:mm").tz(tz);
     console.log("tmp", tmp);
     time.push(-dayjs.duration(now.diff(tmp)).asHours());    // negative values correspond to the past
     temperature.push(stationReports[i].temperature);
